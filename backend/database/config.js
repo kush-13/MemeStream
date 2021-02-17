7f6e865797547bb6af75b53379eb7976437bb78e
kush-13 <33835351+kush-13@users.noreply.github.com>
2021-02-17 14:00:48 +0530

    Update config.js

--- backend/database/config.js
+++ backend/database/config.js
@@ -1,4 +1,4 @@
-const dbUrl = 'mongodb+srv://kush:pass@123@cluster0.9gkpk.mongodb.net/meme?retryWrites=true&w=majority';
+const dbUrl = 'mongodb+srv://username:password@cluster0.9gkpk.mongodb.net/meme?retryWrites=true&w=majority';
 const localdbUrl = "mongodb://localhost:27017/meme"
 const dbOptions = { useNewUrlParser: true, useUnifiedTopology: true };
 module.exports.dbUrl = localdbUrl;
